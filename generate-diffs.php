ebb128e526da26869f273889e1c38b8615ba26a0
Semih ERDOGAN <hasansemiherdogan@gmail.com>
2023-02-16 10:43:01 +0300

    update generator script

--- generate-diffs.php
+++ generate-diffs.php
@@ -4,13 +4,6 @@ define('LARAVEL_FOLDER', 'laravel');
 define('DIFF_FOLDER', 'diffs');
 define('TAGS_FILE', 'tags.txt');
 
-/**
- * Generate Diff Files
- *
- * @param  float $v1
- * @param  float $v2
- * @return void
- */
 function generateDiffFile($v1, $v2)
 {
     $file = sprintf('%s/%s...%s.diff', DIFF_FOLDER, $v1, $v2);
@@ -20,12 +13,6 @@ function generateDiffFile($v1, $v2)
     }
 }
 
-/**
- * Tag version formatter.
- *
- * @param  string $v
- * @return string
- */
 function versionFormat($version)
 {
     return str_replace('v', '', $version);
@@ -44,15 +31,18 @@ $versionList = explode(PHP_EOL, trim($versionList));
 
 $excludedVersions = ['v3', 'v4', 'v5.0', 'v5.1', 'v5.2', 'v5.3', 'v5.4'];
 
-$versionList = array_filter($versionList, function ($version) use ($excludedVersions) {
-  foreach ($excludedVersions as $excludedVersion) {
-    if (substr($version, 0, strlen($excludedVersion)) === $excludedVersion) {
-      return false;
-    }
-  }
+$versionList = (array) array_filter(
+    $versionList,
+    function ($version) use ($excludedVersions) {
+        foreach ($excludedVersions as $excludedVersion) {
+            if (substr($version, 0, strlen($excludedVersion)) === $excludedVersion) {
+                return false;
+            }
+        }
 
-  return true;
-});
+        return true;
+    }
+);
 
 $versionList = array_values($versionList);
 
@@ -65,33 +55,29 @@ file_put_contents(TAGS_FILE, implode(PHP_EOL, $versionList));
 foreach ($versionList as $index => $baseVersion) {
     echo $baseVersion.PHP_EOL;
 
-    $latestVersion = null;
+    $versionsToCreate = (array) array_filter(
+        $versionList,
+        function ($version) use ($baseVersion) {
+            $versionToCheck = substr($baseVersion, 0, 2) === 'v5' ?
+                substr($baseVersion, 0, 4) :
+                substr($baseVersion, 0, 2);
 
-    foreach ($versionList as $version) {
-        if ($version === $baseVersion) {
-            continue;
+            return (substr($version, 0, strlen($versionToCheck)) === $versionToCheck) &&
+                (version_compare(versionFormat($baseVersion), versionFormat($version)) === -1);
         }
+    );
 
-        if (explode('.', $version)[0] != explode('.', $baseVersion)[0]) {
-            continue;
+    $nextItemIndex = array_search($versionsToCreate ? end($versionsToCreate) : $baseVersion, $versionList);
+    if ($nextItemIndex !== false) {
+        $nextItemIndex++;
+
+        if (isset($versionList[$nextItemIndex])) {
+            $versionsToCreate[] = $versionList[$nextItemIndex];
         }
-
-        if (version_compare(versionFormat($baseVersion), versionFormat($version)) === 1) {
-            continue;
-        }
-
-        $latestVersion = $version;
-
-        generateDiffFile($baseVersion, $version);
     }
 
-    if (!is_null($latestVersion)) {
-        $vIndex = array_search($latestVersion, $versionList);
-        $vIndex++;
-
-        if (isset($versionList[$vIndex])) {
-            generateDiffFile($baseVersion, $versionList[$vIndex]);
-        };
+    foreach ($versionsToCreate as $versionToCreate) {
+         generateDiffFile($baseVersion, $versionToCreate);
     }
 }
 
